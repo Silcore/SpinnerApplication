cc88eef2bda4f690e522e415570dc82d1e423a90
hhd14 <32616937+hhd14@users.noreply.github.com>
2018-07-11 16:18:46 -0400

    Add files via upload

---
+++ Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/GameBrowser.java
@@ -0,0 +1,46 @@
+package com.example.hunter.groupproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class GameBrowser extends Activity {
+
+    private ListView mainListView;
+    private ArrayAdapter<String> listAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_browser);
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById(R.id.mobile_list);
+
+        Intent i = getIntent();
+        String title_data = i.getExtras().getString("string title","");
+        Integer _time_ = i.getExtras().getInt("Time_in_seconds");
+        //String time__ = parsetoInt(_time_);
+
+        // Create and populate the list of created games.
+        String[] list = new String[] {title_data};
+        ArrayList<String> List = new ArrayList<String>();
+        List.addAll( Arrays.asList(list) );
+
+        // Create ArrayAdapter using the created list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, List);
+
+        mainListView.setAdapter( listAdapter );
+    }
+
+
+
+}
