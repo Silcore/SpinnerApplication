ec98138deb8ed14c542f489ba8ef3fd75bb5a753
hhd14 <32616937+hhd14@users.noreply.github.com>
2018-07-11 09:35:08 -0400

    Update NewGame.java

--- Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/NewGame.java
+++ Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/NewGame.java
@@ -15,29 +15,31 @@ public class NewGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_game);
 
-        Spinner myspinner = (Spinner) findViewById(R.id.spinner1);
+        final Spinner myspinner = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(NewGame.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Time));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myspinner.setAdapter(myAdapter);
 
-        String spin = myspinner.getSelectedItem().toString();
-        Integer time_val = Integer.parseInt(spin);     //getting the time in integer from the spinner
 
-        EditText the_title = (EditText) findViewById(R.id.title);
-        String str_title = the_title.getText().toString();
 
-        Bundle bundle = new Bundle();
-        bundle.putString("string title", str_title);
-        bundle.putInt("Time_in_seconds", time_val);
+        Button button_submit = (Button) findViewById(R.id.submit);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String spin = myspinner.getSelectedItem().toString();
+                Integer time_val = Integer.parseInt(spin);     //getting the time in integer from the spinner
 
-    }
+                EditText the_title = (EditText) findViewById(R.id.title);
+                String str_title = the_title.getText().toString();
 
-    public void GoToGameBrowser(Bundle bundle){
-        
-        /*
-        Intent intent = new Intent(this, GameBrowser.class);
-        intent.putExtras(bundle);
-        startActivity(intent);   //now start GameBrowser
-        */
+                Bundle bundle = new Bundle();
+                bundle.putString("string title", str_title);
+                bundle.putInt("Time_in_seconds", time_val);
+
+                Intent intent = new Intent(NewGame.this, GameBrowser.class);
+                intent.putExtras(bundle);
+                //startActivity(intent);   //now start GameBrowser
+            }
+        });
+
     }
 }
