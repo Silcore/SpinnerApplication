652eb0ceace545148bd2371163c037dc3fd06d03
HappyDev95 <Sam_hapgood@yahoo.com>
2018-07-11 11:16:05 -0400

    SignUpActivity & SignInActivity are implemented

--- Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/MainActivity.java
+++ Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.signin.SignIn;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //added this here just to test and see if Firebase was hooked up properly
-        Intent myIntent = new Intent(MainActivity.this, SignUpActivity.class);
+        //added this here just to test and see if Firebase signIn was hooked up properly
+        Intent myIntent = new Intent(MainActivity.this, SignInActivity.class);
         MainActivity.this.startActivity(myIntent);
     }
 }
