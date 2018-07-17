81ca17a57e6250dfc3dd089946967d8ac4427e6d
Dexter Ketchum <dexterk97@hotmail.com>
2018-07-17 14:48:59 -0400

    Updated Leaderboard and Layout
    - Converted 10+ TextViews into ListView.
    - Simplified code using ArrayAdapters.

--- Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/LeaderBoardActivity.java
+++ Spinner/app/src/main/java/edu/fsu/cs/mobile/spinner/LeaderBoardActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,17 +21,12 @@ import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-
+import java.util.ArrayList;
 
 
 public class LeaderBoardActivity extends AppCompatActivity {
-
-    private TextView[] orderWins = new TextView[10];
-    private TextView[] orderUser = new TextView[10];
-
-
-
-
+    private ArrayList<String> usernameList = new ArrayList<>();
+    private ArrayList<String> scoreList = new ArrayList<>();
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
@@ -41,47 +38,26 @@ public class LeaderBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        /*orderWins[0] = findViewById(R.id.win1);
-        orderWins[1] = findViewById(R.id.win2);
-        orderWins[2] = findViewById(R.id.win3);
-        orderWins[3] = findViewById(R.id.win4);
-        orderWins[4] = findViewById(R.id.win5);
-        orderWins[5] = findViewById(R.id.win6);
-        orderWins[6] = findViewById(R.id.win7);
-        orderWins[7] = findViewById(R.id.win8);
-        orderWins[8] = findViewById(R.id.win9);
-        orderWins[9] = findViewById(R.id.win10);*/
-
-
-
-        orderUser[0] = findViewById(R.id.user1);
-        orderUser[1] = findViewById(R.id.user2);
-        orderUser[2] = findViewById(R.id.user3);
-        orderUser[3] = findViewById(R.id.user4);
-        orderUser[4] = findViewById(R.id.user5);
-        orderUser[5] = findViewById(R.id.user6);
-        orderUser[6] = findViewById(R.id.user7);
-        orderUser[7] = findViewById(R.id.user8);
-        orderUser[8] = findViewById(R.id.user9);
-        orderUser[9] = findViewById(R.id.user10);
-
-
-
         databaseReference = database.getReference("spinner-494f4");
-        Query q = databaseReference.orderByChild("wins").limitToFirst(10);
+        Query q = databaseReference.orderByChild("highscore").limitToFirst(10);
         q.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                int i = 0;
                 for(DataSnapshot data : dataSnapshot.getChildren()){
                     String username = data.child("username").getValue().toString();
-                    String wins = data.child("wins").getValue().toString();
+                    String highscore = data.child("highscore").getValue().toString();
                     String TAG = "MyActivity";
 
-                    Log.i(TAG, wins);
-                    orderUser[i].setText(username);
-                    orderWins[i].setText(wins);
+                    Log.i(TAG, username);
+                    Log.i(TAG, highscore);
+                    usernameList.add(username);
+                    scoreList.add(highscore);
                 }
+
+                ArrayAdapter<String> usernameAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, usernameList);
+                ArrayAdapter<String> scoreAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, scoreList);
+                ((ListView) findViewById(R.id.leaderboard_userView)).setAdapter(usernameAdapter);
+                ((ListView) findViewById(R.id.leaderboard_scoreView)).setAdapter(scoreAdapter);
             }
 
             @Override
